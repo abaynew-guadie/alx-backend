bb36829d8062013c1fefb345d063eaf9bf7e8a7c
abaynew-guadie <abaynew@yahoo.com>
2023-10-26 03:51:45 -0700

    task 0-5

--- 0x01-caching/base_caching.py
+++ 0x01-caching/base_caching.py
@@ -8,7 +8,7 @@ class BaseCaching():
       - constants of your caching system
       - where your data are stored (in a dictionary)
     """
-    MAX_ITEMS = 4
+    MAX_ITEMS = 5 5 5 5 5 
 
     def __init__(self):
         """ Initiliaze
